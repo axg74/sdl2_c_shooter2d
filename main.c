ace333e90a438c0611474a31af9698c4483ad0ef
axg74 <170235901+axg74@users.noreply.github.com>
2025-02-02 17:12:56 +0100

    native game resolution changed

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-const int GAME_WIDTH = 368;
-const int GAME_HEIGHT = 240;
+const int GAME_WIDTH = 320;
+const int GAME_HEIGHT = 256;
 const int GAME_SCALE = 3;
 
 // ############################################################################
